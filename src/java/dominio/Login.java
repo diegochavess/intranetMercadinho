998c949e4e920e772dba629e32cb4709282b0edf
Renan <Renan@DESKTOP-49CKTNN.home>
2018-06-12 21:16:42 -0300

    -renan- apenas falta o type na linha 72

---
+++ src/java/dominio/Login.java
@@ -0,0 +1,32 @@
+
+package dominio;
+
+public class Login {
+    private String login;
+    private String senha;
+    private String novaSenha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNovaSenha() {
+        return novaSenha;
+    }
+
+    public void setNovaSenha(String novaSenha) {
+        this.novaSenha = novaSenha;
+    }
+}
