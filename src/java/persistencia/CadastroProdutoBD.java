2c090b82bc3e82462684617795a6174dd47c8dc8
felip <felip@DESKTOP-NA96MT1>
2018-06-18 17:05:06 -0300

    cadastro de produtos

--- src/java/persistencia/CadastroProdutoBD.java
+++ src/java/persistencia/CadastroProdutoBD.java
@@ -9,40 +9,69 @@ import java.util.ArrayList;
 public class CadastroProdutoBD {
 
     private static final String caminho = "C:\\Users\\felip\\Desktop\\novo projeto\\intranetMercadinho\\";
+   
     private static ArrayList<CadastroProduto> lista = new ArrayList<CadastroProduto>();
-
-    public static void inserir(CadastroProduto novoCadastroProduto) {
+    
+    public static void inserir(CadastroProduto cadastroProduto){
+        lerXml(); //lê o XML e preenche a lista de Produtos
+        int ultimaPosicao = lista.size()-1;
+        if (ultimaPosicao >= 0){
+            CadastroProduto ultimoCadastroProduto = lista.get(ultimaPosicao);
+            cadastroProduto.setCodigoProduto(ultimoCadastroProduto.getCodigoProduto()+1);
+        }else{
+            cadastroProduto.setCodigoProduto(1);
+        }
+        lista.add(cadastroProduto); //adiciona um produto no final da lista
+        salvarXml(); //atualiza o xml com o que tem na lista
+    }
+    
+    public static void alterar(CadastroProduto cadastroProduto){
         lerXml();
-        lista.add(novoCadastroProduto);
+        excluir(cadastroProduto.getCodigoProduto());
+        inserir(cadastroProduto);
         salvarXml();
     }
-
-    public static void alterar(CadastroProduto cadastroProdutoAlterado) {
-        excluir(cadastroProdutoAlterado.getCodigoProduto());
-        inserir(cadastroProdutoAlterado);
-
-    }
-
-    public static void excluir(int codigoProduto) {
-        for (int i = 0; i < lista.size(); i++) {
-            CadastroProduto cadaEstoque = lista.get(i);
-            if (cadaEstoque.getCodigoProduto()==codigoProduto) {
+    
+ 
+    public static void excluir(int codigoProduto){
+        lerXml();
+        for(int i=0; i < lista.size(); i++){
+            CadastroProduto cadaCadastroProduto = lista.get(i);
+            
+           
+            if (cadaCadastroProduto.getCodigoProduto()== codigoProduto){
                 lista.remove(i);
+            }
+        }
+        salvarXml();
+    }
+    public static ArrayList<CadastroProduto> listar(){     
+        lerXml();
+        //retorna todos os objetos do banco de dados
+        return lista;
+    }
+    
+    public static CadastroProduto getCodigoProduto(int cogigoProduto){
+        lerXml();
+        CadastroProduto cadastroProdutoEncontrado = null;
+        for(int i=0; i < lista.size(); i++){
+            CadastroProduto cadaCadastroProduto = lista.get(i);
+            
+            
+            if (cadaCadastroProduto.getCodigoProduto()== (cogigoProduto)){
+                cadastroProdutoEncontrado= cadaCadastroProduto;
                 break;
             }
         }
+        return cadastroProdutoEncontrado;
     }
 
-    public static ArrayList<CadastroProduto> listar() {
-           return lista;
-           
-    }
- private static void lerXml(){
-        File arquivo=new File(caminho + "CadastroProduto.xml");
+    private static void lerXml(){
+        File arquivo=new File(caminho + "cadastroProduto.xml");
         if (arquivo.exists()){
             //armazenar XML no vetor
             XStream xstream=new XStream();
-            xstream.alias("estoque",CadastroProduto.class);
+            xstream.alias("cadastroProduto",CadastroProduto.class);
             lista = (ArrayList<CadastroProduto>) xstream.fromXML(arquivo);
         }else{
             lista = new ArrayList<CadastroProduto>();
@@ -51,9 +80,9 @@ public class CadastroProdutoBD {
     
     private static void salvarXml(){
         XStream xstream = new XStream();
-        xstream.alias("estoque",CadastroProduto.class);
+        xstream.alias("cadastroProduto",CadastroProduto.class);
         try{
-            FileWriter escritor=new FileWriter(caminho + "CadastroProduto.xml");
+            FileWriter escritor=new FileWriter(caminho + "cadastroProduto.xml");
             escritor.write( xstream.toXML(lista) );
             escritor.close();
         }catch(Exception ex){
